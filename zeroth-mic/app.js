624b561847416708a8dfff5bf2cf1cc5207e7dc4
Carter <kattergil331@gmail.com>
2018-10-16 10:37:12 +0900

    Add word confidence to examples

--- zeroth-mic/app.js
+++ zeroth-mic/app.js
@@ -1,19 +1,11 @@
 const start = document.querySelector('.start');
 const stop = document.querySelector('.stop');
 const transcript = document.querySelector('.transcript');
+const confidence = document.querySelector('.confidence');
 const json = document.querySelector('.json');
 stop.disabled = true;
 
-const params = {
-  key: 'YOUR_API_KEY',
-  language: 'kor',
-  finalOnly: false,
-  ws: false,
-  debug: true
-};
-
-const zeroth = new Zeroth.ZerothMic(params);
-
+let zeroth = null;
 const onError = err => {
   transcript.textContent = 'getUserMedia error: ' + err;
 };
@@ -25,6 +17,14 @@ const onSuccess = () => {
 
   zeroth.ondata = data => {
     transcript.textContent = data.transcript;
+    if (data.final) {
+      confidence.innerHTML = data['word-alignment']
+        .map(
+          result =>
+            `<span style="opacity: ${result.confidence}; font-weight:bold">${result.word}</span>`
+        )
+        .join(' ');
+    }
     json.textContent = JSON.stringify(data);
   };
 
@@ -39,6 +39,15 @@ const onSuccess = () => {
 };
 
 start.onclick = () => {
+  const params = {
+    key: document.querySelector('#key').value,
+    language: 'kor',
+    finalOnly: false,
+    ws: false,
+    debug: true
+  };
+
+  zeroth = new Zeroth.ZerothMic(params);
   zeroth
     .start()
     .then(onSuccess)
